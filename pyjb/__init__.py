f0d612b6f796382041751f37ae462dfb34e5cfdd
Quentin Ågren <quentin@scilifelab.se>
2024-05-06 11:42:56 +0200

    Display local BED file

--- pyjb/__init__.py
+++ pyjb/__init__.py
@@ -1,7 +1,8 @@
 import anywidget
+import traitlets
 from pathlib import Path
 
 class LGVWidget(anywidget.AnyWidget):
     _esm = Path(__file__).parent / 'static' / 'index.mjs'
-    
+    track = traitlets.Unicode().tag(sync=True)
 
--- src/index.js
+++ src/index.js
@@ -8,33 +8,34 @@ import { createElement } from 'https://esm.sh/react'
 import { createRoot } from 'https://esm.sh/react-dom/client';
 
 const assembly = {
-  name: 'GRCh38',
-  sequence: {
-    type: 'ReferenceSequenceTrack',
-    trackId: 'GRCh38-ReferenceSequenceTrack',
-    adapter: {
-      type: 'BgzipFastaAdapter',
-      fastaLocation: {
-        uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/fasta/GRCh38.fa.gz',
-      },
-      faiLocation: {
-        uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/fasta/GRCh38.fa.gz.fai',
-      },
-      gziLocation: {
-        uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/fasta/GRCh38.fa.gz.gzi',
-      },
+    name: 'GRCh38',
+    sequence: {
+	type: 'ReferenceSequenceTrack',
+	trackId: 'GRCh38-ReferenceSequenceTrack',
+	adapter: {
+	    type: 'BgzipFastaAdapter',
+	    fastaLocation: {
+		uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/fasta/GRCh38.fa.gz',
+	    },
+	    faiLocation: {
+		uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/fasta/GRCh38.fa.gz.fai',
+	    },
+	    gziLocation: {
+		uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/fasta/GRCh38.fa.gz.gzi',
+	    },
+	},
     },
-  },
-  aliases: ['hg38'],
-  refNameAliases: {
-    adapter: {
-      type: 'RefNameAliasAdapter',
-      location: {
-        uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/hg38_aliases.txt',
-      },
+    aliases: ['hg38'],
+    refNameAliases: {
+	adapter: {
+	    type: 'RefNameAliasAdapter',
+	    location: {
+		uri: 'https://s3.amazonaws.com/jbrowse.org/genomes/GRCh38/hg38_aliases.txt',
+	    },
+	},
     },
-  },
 };
+
 
 const defaultSession = {
     name: 'Default',
@@ -57,16 +58,32 @@ const defaultSession = {
 };
 
 
+function bedTrack(name, path) {
+    return {
+	name,
+	type: "FeatureTrack",
+	trackId: name.toLowerCase(),
+	adapter: {
+	    type: 'BedAdapter',
+	    bedLocation: {uri: path}
+	}
+    }
+}
+
 function render({model, el}) {
     console.log("Rendering linear genome view.");
-    const state = createViewState({
-	assembly,
-	location: '10:29,838,758..29,838,798',
-	defaultSession
+    const bedPath = model.get('track');
+    model.widget_manager.resolveUrl(bedPath).then(bedUrl => {
+	const state = createViewState({
+	    assembly,
+	    location: '10:29,838,758..29,838,798',
+	    tracks: [bedTrack('Minibed', bedUrl)],
+	    defaultSession
+	});
+	const root = createRoot(el);
+	const lgv = createElement(JBrowseLinearGenomeView, { viewState: state });
+	root.render(lgv);
     });
-    const root = createRoot(el);
-    const lgv = createElement(JBrowseLinearGenomeView, { viewState: state });
-    root.render(lgv);
 }
 
 
