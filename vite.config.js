8f14bace9eda668b11592b08f60aa5ca74060e0b
Quentin Ågren <quentin@scilifelab.se>
2024-05-24 11:48:19 +0200

    Polyfills for Vite's development server

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
 import { resolve } from 'path'
-import { defineConfig } from "vite";
-import anywidget from "@anywidget/vite"
+import { defineConfig } from 'vite';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import anywidget from '@anywidget/vite'
 import react from '@vitejs/plugin-react'
 
 
 export default defineConfig({
     build: {
-	outDir: resolve(__dirname, "pyjb/static"),
+	outDir: resolve(__dirname, 'pyjb/static'),
 	lib: {
-	    entry: resolve(__dirname, "src/index.js"),
+	    entry: resolve(__dirname, 'src/index.js'),
 	    fileName: 'index',
-	    formats: ["es"],
+	    formats: ['es'],
 	},
 	rollupOptions: {
 	    // ipywidget expects a self-contained ESM file
@@ -24,5 +25,21 @@ export default defineConfig({
 	// output bundle.
 	'process.env.NODE_ENV': '"production"'
     },
-    plugins: [anywidget(), react()]
+    plugins: [
+	anywidget(),
+	react(),
+    ],
+    optimizeDeps: {
+	esbuildOptions: {
+	    define: {
+		global: 'globalThis'
+	    },
+	    plugins: [
+		NodeGlobalsPolyfillPlugin({
+		    buffer: true,
+		    global: true
+		})]
+	}
+    }
+
 });
